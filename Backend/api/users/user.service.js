e3c56abacc4da84e8136d6234cfe78f57b675029
bernadett1104 <bernadett11040202@gmail.com>
2022-03-31 13:43:56 +0200

    03__Users done

--- Backend/api/users/user.service.js
+++ Backend/api/users/user.service.js
@@ -24,4 +24,48 @@ module.exports = {
             return callBack(null, results[0]);
         });
     },
+    getUsers: callBack => {
+        const queryString = `select * from users`;
+        const params = [];
+        pool.query(queryString, params, (error, results, fields) => {
+            if (error) {
+                return callBack(error);
+
+            }
+            return callBack(null, results);
+        });
+    },
+    getUserByUserId: (id, callBack) => {
+        const queryString = `Select * from users Where id= ?`;
+        const params = [id];
+        pool.query(queryString, params, (error, results, fields) => {
+            if (error) {
+                return callBack(error);
+
+            }
+            return callBack(null, results);
+        });
+    },
+    updateUser: (data, callBack) => {
+        const queryString = `Update users set name=?, email=?, password=?, permission=? Where id=?`;
+        const params = Object.values(data);
+        pool.query(queryString, params, (error, results, fields) => {
+            console.log(params, queryString, results);
+            if (error) {
+                return callBack(error);
+
+            }
+            return callBack(null, results);
+        });
+    },
+    deleteUser: (data, callBack) => {
+        const queryString = `Delete from users where id = ?`;
+        const params = [data.id];
+        pool.query(queryString, params, (error, results, fields) => {
+            if (error) {
+                return callBack(error);
+            }
+            return callBack(null, results);
+        });
+    },
 };
